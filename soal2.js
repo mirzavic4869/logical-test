c36b7e64614421181186eaa94b13b69ad47df4a6
mirzavic4869 <avicenamirzam@gmail.com>
2022-10-09 15:49:17 +0700

    split

--- soal2.js
+++ soal2.js
@@ -1,7 +1,18 @@
+// function countWords(str) {
+// 	const arr = str.split(" ");
+// 	var specialChars = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/gi;
+// 	return arr.filter((word) => word !== specialChars).length;
+// }
+
+// console.log(countWords("Saat meng*ecat tembok, Agung dib_antu oleh Raihan."));
+
+// var sectionToCheck = "Saat meng*ecat tembok, Agung dib_antu oleh Raihan.";
+// var allFoundCharacters = sectionToCheck.match(/[@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/g);
+// console.log(allFoundCharacters.length); //count
+
 function countWords(str) {
 	const arr = str.split(" ");
 
-	return arr.filter((word) => word !== "*").length;
+	return arr.filter((word) => word !== "").length;
 }
-
 console.log(countWords("Saat meng*ecat tembok, Agung dib_antu oleh Raihan."));
